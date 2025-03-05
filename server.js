248774dbaa5917edc1df19005cd0733a2950362f
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2025-03-05 16:57:37 +0000

    Adding sql files, Js logic and updating yaml

--- server.js
+++ server.js
@@ -1,2 +1,18 @@
 console.log("User:", process.env.USER);
 console.log("Password:", process.env.PASSWORD);
+
+const mysql = require('mysql2/promise');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.json());
+
+const pool = mysql.createPool({
+    host: 'your_database_host',
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: 'your_database_name',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
