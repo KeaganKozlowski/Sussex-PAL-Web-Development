ea0d44d93301d59bd8f778acbafaef720d474993
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2025-03-10 15:19:51 +0000

    Adding todo for one of webscrapper scripts

--- Scraper for Applications/Linkedin.js
+++ Scraper for Applications/Linkedin.js
@@ -1,26 +1,29 @@
+//Now that the repository variables work in the server file, will work on setting up the email and password
+
+
 require("dotenv").config();
 const puppeteer = require("puppeteer");
 
-// Getting login details from another folder
+//Getting login details from another folder
 
 async function scrapeLinkedIn() {
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-    // Navigate to LinkedIn Login Page
+    //Navigate to LinkedIn Login Page
     await page.goto("https://www.linkedin.com/login");
 
-    // Enter login credentials
+    //Enter login credentials
     await page.type("#username", process.env.LINKEDIN_EMAIL);
     await page.type("#password", process.env.LINKEDIN_PASSWORD);
     await page.click('[type="submit"]');
     await page.waitForNavigation();
 
-    // Go to job search page
+    //Go to job search page
     await page.goto("https://www.linkedin.com/jobs/search/?keywords=Software%20Engineer");
     await page.waitForSelector(".base-search-card__title");
 
-    // Extract job titles
+    //Extract job titles
     const jobs = await page.evaluate(() =>
         Array.from(document.querySelectorAll(".base-search-card__title")).map((job) => job.innerText.trim())
     );
