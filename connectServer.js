e673f68c5cf405f1567d07dd4dc30349b2a84775
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2025-03-20 14:45:32 +0000

    Can add data now
    Add functionality to shutdown the server and take data from json file

---
+++ connectServer.js
@@ -0,0 +1,24 @@
+//Add Data and connect
+
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: 'krier.uscs.sussex.ac.uk',
+    user: 'kk544',
+    password: 'Mysql_627115',
+    database: 'kk544',
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  var sql = "INSERT INTO Jobs (Title, Company, Location) VALUES ('Senior Software Engineer', 'Red Spike Studios', 'Brighton')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+});
+/*
+con.shutdow(function){
+    ServiceWorkerRegistration.
+}*/
