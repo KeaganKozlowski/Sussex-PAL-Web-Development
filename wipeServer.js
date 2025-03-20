f6c9e81cadcebdb442a19ff78eb369ec8840f77e
KeaganKozlowski <91627527+KeaganKozlowski@users.noreply.github.com>
2025-03-20 14:53:38 +0000

    Close connection to server and wipe all data

---
+++ wipeServer.js
@@ -0,0 +1,35 @@
+//Wipe database of all data
+var mysql = require('mysql2');
+
+var con = mysql.createConnection({
+    host: 'krier.uscs.sussex.ac.uk',
+    user: 'kk544',
+    password: 'Mysql_627115',
+    database: 'kk544',
+});
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the database');
+  
+    // Query to get all table names
+    const query = "SHOW TABLES";
+  
+    con.query(query, (err, results) => {
+      if (err) throw err;
+  
+      // Loop through each table and truncate it
+      results.forEach((row) => {
+        const tableName = row[`Tables_in_${con.config.database}`];
+        const truncateQuery = `TRUNCATE TABLE ${tableName}`;
+  
+        con.query(truncateQuery, (err) => {
+          if (err) throw err;
+          console.log(`Truncated table: ${tableName}`);
+        });
+      });
+  
+      // Close the connection
+      con.end();
+    });
+  });
